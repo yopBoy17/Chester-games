b093157126e191a2293acbf071b9fc9c86a49f82
azar <valery.azarov@t2.ru>
2026-09-03 14:38:10 +0300

    fix

--- factory/update-check.js
+++ factory/update-check.js
@@ -1,5 +1,5 @@
 (() => {
-  const CLIENT_VERSION = '2026.09.03.1';
+  const CLIENT_VERSION = '2026.09.03.2';
   const CHECK_INTERVAL_MS = 60_000;
   let updateAvailable = false;
   let reloading = false;

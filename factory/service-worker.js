9e3f16a1c7c6d19d7c0a8a5ef403f02ad8d22eda
azar <valery.azarov@t2.ru>
2026-09-03 14:55:42 +0300

    fix

--- factory/service-worker.js
+++ factory/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'factory-pwa-v6';
+const CACHE_NAME = 'factory-pwa-v7';
 const APP_SHELL = [
   './',
   './index.html',
--- factory/update-check.js
+++ factory/update-check.js
@@ -1,5 +1,5 @@
 (() => {
-  const CLIENT_VERSION = '2026.09.03.4';
+  const CLIENT_VERSION = '2026.09.03.5';
   const CHECK_INTERVAL_MS = 60_000;
   let updateAvailable = false;
   let reloading = false;
